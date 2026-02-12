fix sign error on pressure bc

Exercises/fem/src/fem/element.py
@@ -175,7 +175,7 @@ class IsoparametricElement(Element):
                     traction = np.asarray(sload.value)
                 elif sload.load_type == PRESSURE:
                     n = self.edge_normal(sload.edge, p, xi)
-                    traction = float(sload.value) * n
+                    traction = -float(sload.value) * n
                 else:
                     traction = np.asarray(sload.value)
                 st = self.ref_edge_coords(sload.edge, xi)

Exercises/fem/src/fem/solver.py
@@ -58,7 +58,7 @@ class DirectSolver(Solver):
         d = np.zeros(n + m)
         d[ddofs] = dvals
         d[fdofs] = np.linalg.solve(
-            K[np.ix_(fdofs, fdofs)], R[fdofs] - np.dot(K[np.ix_(fdofs, ddofs)], d[ddofs])
+            K[np.ix_(fdofs, fdofs)], -R[fdofs] - np.dot(K[np.ix_(fdofs, ddofs)], d[ddofs])
         )
 
         react = np.zeros_like(R[:n])